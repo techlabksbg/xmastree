3dfed0f1262b183fce6b77518ed7a75e2cb6f8d3
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-11-18 11:26:37 +0100

    some vec ops, untested

--- src/utils.cpp
+++ src/utils.cpp
@@ -24,3 +24,82 @@ int scale(float f, int color) {
   }
   return r;
 }
+
+
+
+// Simple vector library, acting soley on float[3] objects
+
+/**
+ * Copy from src to dst
+ * @param src Source
+ * @param dst Destination
+ * @returns dst (Pointer to result)
+ */
+float* vec_copy(float *dst, float *src) {
+  for (int i=0; i<3; i++) {
+    dst[i] = src[i];
+  }
+  return dst;
+}
+
+float* vec_add(float *dst, float *src) {
+   for (int i=0; i<3; i++) {
+    dst[i]+= src[i];
+  }
+  return dst;
+}
+
+float* vec_add_null(float *dst, float *src, float mul) {
+   for (int i=0; i<3; i++) {
+    dst[i]+= src[i]*mul;
+  }
+  return dst;
+}
+
+float vec_norm2(float *src) {
+  float l = 0.0;
+  for (int i=0; i<3; i++) {
+    l+=src[i]*src[i];
+  }
+  return l;
+}
+
+float vec_norm(float *src) {
+  return sqrt(vec_norm2(src));
+}
+
+float vec_dot(float *src1, float *src2) {
+  float d = 0.0;
+  for (int i=0; i<3; i++) {
+    d+=src1[i]*src2[i];
+  }
+  return d;
+}
+
+
+float* vec_rotxy(float *dst, float *src, float w) {
+  float c = cos(w);
+  float s = sin(w);
+  if (dst==src) {
+    float x = c*src[0]-s*src[1];
+    dst[1] = s*src[0]+c*src[1];
+    dst[0] = x;
+  } else {
+    dst[0] = c*src[0]-s*src[1];
+    dst[1] = s*src[0]+c*src[1];
+  }
+}
+
+/**
+ * Cross product. Destination must be different from src1 and src2
+ * @param src1 1st vector
+ * @param src2 2nd vector
+ * @param dst result vector
+ * @returns result vector
+ */
+float* vec_cross(float* dst, float* src1, float* src2) {
+  dst[0] = src1[1]*src2[2]-src1[2]*src2[1];
+  dst[1] = src1[2]*src2[0]-src1[0]*src2[2];
+  dst[2] = src1[0]*src2[1]-src1[1]*src1[0];
+  return dst;  
+}
