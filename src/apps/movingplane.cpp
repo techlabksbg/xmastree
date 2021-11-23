113c366e11c6bb462d305a3f7649f91e06f6e2c0
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-11-23 21:34:55 +0100

    Added Alternating axes, good params

--- src/apps/movingplane.cpp
+++ src/apps/movingplane.cpp
@@ -7,14 +7,36 @@ class MovingPlane : App {
     public:
     virtual void loop();
     virtual const char* buttonName() { return "Up'n down"; }
+    virtual bool setGoodParams();
 };
+
+bool MovingPlane::setGoodParams() {
+    static int p = 0;
+    params.speed = 200;
+    params.brightness = 50;
+    switch (p) {
+    case 0:
+        params.color1 = 0x1111ff;
+        params.color2 = 0xff2266;
+        break;
+    case 1:
+        params.color1 = 0xff0000;
+        params.color2 = 0x00ff00;
+        params.speed = 100;
+        break;
+
+    }
+    p = (p+1)%2;
+    return true;
+}
 
 
 void MovingPlane::loop() {
-    float normal[3] {0,0,1};
     float t = secs()/fmap(params.speed, 0, 255, 20, 1.0);
+    int dir = ((int)t) % 3;
+    float normal[3] {dir==0, dir==1, dir==2};
+    float d = sin(t*2*PI)*(params.maxs[dir]-params.mins[dir])/2+(params.maxs[dir]+params.mins[dir])/2;
 
-    float d = sin(t*2*PI)*75+110.0;
     for (int i=0; i<NUMPIXEL; i++) {
         float sp = normal[0]*params.posdata[i][0]+normal[1]*params.posdata[i][1]+normal[2]*params.posdata[i][2]-d;
         params.pixels->setPixelColor(i, sp>0 ? params.color1 : params.color2);
