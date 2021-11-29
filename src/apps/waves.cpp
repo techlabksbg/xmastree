7e550df61d1646b43c798a5b1370648551c99768
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-11-29 15:43:10 +0100

    waves going out instead of in

--- src/apps/waves.cpp
+++ src/apps/waves.cpp
@@ -23,7 +23,7 @@ void Wave::loop() {
         float y = params.posdata[i][1];
         float z = params.posdata[i][2];
         float d = sqrt(x*x+y*y);
-        float f = sin(t*6+d/10)*40/(1+d*d/500)+60+sin(t)*20;
+        float f = sin(-t*6+d/10)*40/(1+d*d/500)+60+sin(t)*20;
         int h = fmod(t/4+d/1000.0,1.0)*0xffff;
         int v = z<f ? 255 : 0;
         params.pixels->setPixelColor(i, params.pixels->ColorHSV(h,255,v));
