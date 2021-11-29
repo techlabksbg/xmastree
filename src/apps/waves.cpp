cfa733518b828368135923d934ba4d62c596cecd
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-11-29 12:21:14 +0100

    should look like something now

--- src/apps/waves.cpp
+++ src/apps/waves.cpp
@@ -17,13 +17,13 @@ bool Wave::setGoodParams() {
 }
 
 void Wave::loop() {
-    float t = fmod(secs()/fmap(params.speed, 0, 255, 40, 2.0),1.0);
+    float t = secs()/fmap(params.speed, 0, 255, 40, 1.0);
     for (int i=0; i<NUMPIXEL; i++) {
         float x = params.posdata[i][0];
         float y = params.posdata[i][1];
         float z = params.posdata[i][2];
         float d = sqrt(x*x+y*y);
-        float f = sin(d/10)*20/(1+d*d/500)+60+sin(t/5);
+        float f = sin(t*6+d/10)*40/(1+d*d/500)+60+sin(t)*20;
         int h = fmod(t/4+d/1000.0,1.0)*0xffff;
         int v = z<f ? 255 : 0;
         params.pixels->setPixelColor(i, params.pixels->ColorHSV(h,255,v));
