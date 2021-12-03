ff2307a75a8b59d86867eb43bb3c273de9a80ad2
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-12-03 20:08:18 +0100

    hsv correction

--- src/apps/waves.cpp
+++ src/apps/waves.cpp
@@ -26,7 +26,7 @@ void Wave::loop() {
         float f = sin(-t*6+d/10)*40/(1+d*d/500)+60+sin(t)*20;
         int h = fmod(t/4+d/1000.0,1.0)*0xffff;
         float v = z<f ? 0.5f : 0.0f;
-        params.pixels->SetPixelColor(i, HslColor(h,255,v));
+        params.pixels->SetPixelColor(i, HslColor(h,1.0f,v));
     }
     params.pixels->Show();
 }
