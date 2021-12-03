6213fad9c05287b7f711fcc96f471ea0fd5b21c6
Ivo Blöchliger <ivo.bloechliger@ksbg.ch>
2021-12-03 21:07:08 +0100

    removed debug messages

--- src/apps/funkeln.cpp
+++ src/apps/funkeln.cpp
@@ -45,7 +45,6 @@ void Funkeln::loop() {
         for (int i=0; i<NUMFUNK; i++) {
             pos[i] = random(NUMPIXEL);
             colors[i] = mix(random(10001)/10000.0);
-            Serial.printf("mix to %0x\n", HtmlColor(colors[i]).Color);
             //Serial.printf("colors[%d]=%06x\n",i,colors[i]);
             count[i] = 0;
             len[i] = (70+random(60))*((255-params.speed)+20)/120.0;
@@ -57,10 +56,6 @@ void Funkeln::loop() {
             t = t<0.5 ? 2.0*t : 2.0-2*t;
             t = pow(t,4);
             RgbColor c = scale(t,colors[i]);
-            /*if (i==0) {
-                Serial.printf("count=%d, len=%d, t=%f, colors[0]=%06x, c=%06x\n", count[i], len[i], t, colors[i], c);
-            }*/
-            params.pixels->SetPixelColor(pos[i], c);
             if (count[i]>=len[i]) {
                 pos[i] = random(NUMPIXEL);
                 colors[i] = mix(random(10001)/10000.0);
